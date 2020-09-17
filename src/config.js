4434f83a155729717d0054e0d4c35775ee49cf8c
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-17 16:31:28 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN: process.env.API_TOKEN || null,
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 };
